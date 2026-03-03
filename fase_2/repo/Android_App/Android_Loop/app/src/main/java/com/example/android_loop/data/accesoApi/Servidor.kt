656a61b670203325a2b48fa206d39f20eb0a2f4f
Fabiana <fabhunalv@alu.edu.gva.es>
2026-03-03 17:05:20 +0100

    Cambio servidor

--- fase_2/repo/Android_App/Android_Loop/app/src/main/java/com/example/android_loop/data/accesoApi/Servidor.kt
+++ fase_2/repo/Android_App/Android_Loop/app/src/main/java/com/example/android_loop/data/accesoApi/Servidor.kt
@@ -4,8 +4,8 @@ object Servidor {
 
     // http://10.0.2.2:8069      ← solo funciona en emulador
     // http://192.168.X.X:8069  ← dispositivo físico (cambia X.X por tu IP local)
-    // val BASE_URL = "http://192.168.51.1:8069"
+     val BASE_URL = "http://192.168.51.1:8069"
 
-     val BASE_URL = "http://localhost:8069"  // Solo para pruebas en el emulador
+     //val BASE_URL = "http://localhost:8069"  // Solo para pruebas en el emulador
 
 }
