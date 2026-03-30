5fe90ac9fdf728ce8c35f0a4b5e5a226eb855fde
Andrea Git <andrius.data@gmail.com>
2026-03-30 10:07:36 +0200

    Merge resuelto

--- fase_2/repo/Android_App/Android_Loop/app/src/main/java/com/example/android_loop/data/accesoApi/Servidor.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android_loop.data.accesoApi
-
-object Servidor {
-     //val BASE_URL = "http://localhost:8069"  // Solo para pruebas en el emulador
-     val BASE_URL = "http://10.26.32.229:8069"  // IP actual
-}
-
